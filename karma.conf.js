997fe31b268785ddf32b3a8cf34387dcbdf6e009
julioizidoro <jizidoro@globo.com>
2019-09-28 10:21:56 -0300

    laytou kamila

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/sasso-gui'),
+      dir: require('path').join(__dirname, './coverage/sasso-tabacco'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
