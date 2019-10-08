dee6df2aa32daef670b8524f29267152e9ede855
BROGNOLI\naidion.brovedan <brovedan@gmail.com>
2019-10-08 13:06:58 -0300

    telas de clientes

--- src/app/cliente/cadcliente/cadcliente.component.ts
+++ src/app/cliente/cadcliente/cadcliente.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadcliente',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadclienteComponent implements OnInit {
 
-  constructor() { }
+   formulario: FormGroup;
+   pessoaJuridica = false;
+  pessoaFisica = false;
+   isFirstOpen = false;
+   oneAtATime: true;
+   public maskCPF = [/[0-9]/, /[0-9]/, /[0-9]/, '.', /[0-9]/, /[0-9]/, /[0-9]/, '.', /[0-9]/, /[0-9]/, /[0-9]/, '-', /[0-9]/, /[0-9]/];
+  // tslint:disable-next-line:max-line-length
+  public maskCNPJ = [/[0-9]/, /[0-9]/, '.', /[0-9]/, /[0-9]/, /[0-9]/, '.', /[0-9]/, /[0-9]/, /[0-9]/, '/', /[0-9]/, /[0-9]/, /[0-9]/, /[0-9]/, '-', /[0-9]/, /[0-9]/];
+  public maskFONE = ['(', /[0-9]/, /[0-9]/, /[0-9]/, ')', /[0-9]/, /[0-9]/, /[0-9]/, /[0-9]/, '-', /[0-9]/, /[0-9]/, /[0-9]/, /[0-9]/];
+  // tslint:disable-next-line:max-line-length
+  public maskCELULAR = ['(', /[0-9]/, /[0-9]/, /[0-9]/, ')', /[0-9]/, /[0-9]/, /[0-9]/, /[0-9]/, /[0-9]/, '-', /[0-9]/, /[0-9]/, /[0-9]/, /[0-9]/];
+  public maskCEP = [/[0-9]/, /[0-9]/, /[0-9]/, /[0-9]/, /[0-9]/, '-', /[0-9]/, /[0-9]/, /[0-9]/];
+  public maskDATE = [/[0-9]/, /[0-9]/, '/', /[0-9]/, /[0-9]/, '/', /[0-9]/,  /[0-9]/, /[0-9]/, /[0-9]/];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+  }
+
+  setTipoJuridico() {
+    if (this.formulario.get('tipojuridico').value === 'PF') {
+      this.pessoaJuridica = false;
+      this.pessoaFisica = true;
+    } else {
+      this.pessoaJuridica = true;
+      this.pessoaFisica = false;
+    }
   }
 
 }
